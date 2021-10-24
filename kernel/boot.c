96aab53a5999ef4f835283240a1128950ef95b75
Nour-eddine Taleb <contact@noureddine.xyz>
2021-10-24 17:36:04 +0100

    console: remove video mode and opt for to the good old text mode, because video mode stops working when loading gdt (maybe a bug)

--- kernel/boot.c
+++ kernel/boot.c
@@ -8,86 +8,23 @@
 #include <idt.h>
 #include <test.h>
 
-static u8 stack[8192];
 extern void *stivale2_get_tag(struct stivale2_struct *stivale2_struct, u64 id);
 
-static struct stivale2_header_tag_terminal terminal_hdr_tag = {
-	.tag = {
-		.identifier = STIVALE2_HEADER_TAG_TERMINAL_ID,
-		.next = 0},
-	// The terminal header tag possesses a flags field, leave it as 0 for now
-	// as it is unused.
-	.flags = 0};
-
-// We are now going to define a framebuffer header tag.
-// This tag tells the bootloader that we want a graphical framebuffer instead
-// of a CGA-compatible text mode. Omitting this tag will make the bootloader
-// default to text mode, if available.
-static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
-	.tag = {
-		.identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
-		.next = (u64)&terminal_hdr_tag},
-	// We set all the framebuffer specifics to 0 as we want the bootloader
-	// to pick the best it can.
-	.framebuffer_width = 0,
-	.framebuffer_height = 0,
-	.framebuffer_bpp = 0,
-};
-
-// The stivale2 specification says we need to define a "header structure".
-// This structure needs to reside in the .stivale2hdr ELF section in order
-// for the bootloader to find it. We use this __attribute__ directive to
-// tell the compiler to put the following structure in said section.
-__section(".stivale2hdr") __used static struct stivale2_header stivale_hdr = {
-	// The entry_point member is used to specify an alternative entry
-	// point that the bootloader should jump to instead of the executable's
-	// ELF entry point. We do not care about that so we leave it zeroed.
-	.entry_point = 0,
-	// Let's tell the bootloader where our stack is.
-	// We need to add the sizeof(stack) since in x86(_64) the stack grows
-	// downwards.
-	.stack = (u64)((char *)stack + sizeof(stack)),
-	// Bit 1, if set, causes the bootloader to return to us pointers in the
-	// higher half, which we likely want.
-	// Bit 2, if set, tells the bootloader to enable protected memory ranges,
-	// that is, to respect the ELF PHDR mandated permissions for the executable's
-	// segments.
-	.flags = (1 << 0) | (1 << 1),
-	//.flags = (1 << 1) | (1 << 2),
-	// This header structure is the root of the linked list of header tags and
-	// points to the first one in the linked list.
-	.tags = (u64)((void *)&framebuffer_hdr_tag)};
-
-// The following will be our kernel's entry point.
 void _start(struct stivale2_struct *stivale2_struct)
 {
 	// Let's get the terminal structure tag from the bootloader.
-	struct stivale2_struct_tag_terminal *term = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TERMINAL_ID);
+	struct stivale2_struct_tag_textmode *console = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TEXTMODE_ID);
 	struct stivale2_struct_tag_memmap *memmap = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_MEMMAP_ID);
 
-	// Check if the tag was actually found.
-	if (term == NULL)
-	{
-		PANIC("stivale2_struct_tag_terminal expected but not been recieved");
-		// It wasn't found, just hang...
-	}
-
-	if (memmap == NULL)
-	{
-		PANIC("stivale2_struct_tag_memmap expected but not been recieved");
-	}
-
 	// Let's get the address of the terminal write function.
-	init_console((term_write_t *)term->term_write);
+	init_console(console);
 
 	for (u64 i = 0; i < memmap->entries; i++)
-	{
 		if (memmap->memmap[i].type == STIVALE2_MMAP_USABLE)
 		{
 			printk("usable memory = %x length = %d\n", memmap->memmap[i].base, memmap->memmap[i].length);
 			track_region((void *)memmap->memmap[i].base, memmap->memmap[i].length);
 		}
-	}
 
 	gdt_init();
 	idt_init();
--- kernel/include/console.h
+++ kernel/include/console.h
@@ -2,20 +2,34 @@
 #define CONSOLE_H
 
 #include <types.h>
+#include <stivale2.h>
 
-typedef void term_write_t(const char *string, size_t length);
-void init_console(term_write_t *term_hook);
-void printk(const char *fmt, ...);
-
-#define PUTV(var) monitor_putv(#var, (u32)var)
+// printk helpers
 #define KERN_INFO "INFO: "
 #define KERN_WARN "WARN: "
 #define KERN_ERR "ERR: "
 #define KERN_EMERG "EMERG: "
-
 #define pr_info(fmt, ...) printk(KERN_INFO fmt "\n", ##__VA_ARGS__)
 #define pr_warn(fmt, ...) printk(KERN_WARN fmt "\n", ##__VA_ARGS__)
 #define pr_err(fmt, ...) printk(KERN_ERR fmt "\n", ##__VA_ARGS__)
 #define pr_emerg(fmt, ...) printk(KERN_EMERG fmt "\n", ##__VA_ARGS__)
+
+// VGA IO
+#define PORT_VGA_CMD	0x3D4
+#define PORT_VGA_DATA	0x3D5
+#define CMD_VGA_LOW		15
+#define CMD_VGA_HIGH	14
+
+#define VGA_COLOR_BLACK	0
+#define VGA_COLOR_WHITE	15
+#define COLOR(fg, bg) ((fg << 4) | (bg & 0x0f))
+#define DEFAULT_COLOR_SCHEME COLOR(VGA_COLOR_BLACK, VGA_COLOR_WHITE)
+#define VGA_CHAR(c) ((DEFAULT_COLOR_SCHEME << 8) | c)
+
+void init_console(struct stivale2_struct_tag_textmode *);
+void putc(char c);
+void puts(char *str);
+void clear_screen(void);
+void printk(const char *fmt, ...);
 
 #endif // CONSOLE_H
