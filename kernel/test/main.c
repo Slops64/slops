21b74db0b4082bcdeb53ea40aeccb2357328bc77
Pigmy-penguin <pigmy-penguin@protonmail.com>
2021-10-26 09:52:20 +0200

    mm: Testing the pmm

--- kernel/test/main.c
+++ kernel/test/main.c
@@ -4,10 +4,16 @@
 void do_tests(void)
 {
 	char *buf = kmalloc(12);
-	strcpy(buf, "test");
+	strcpy(buf, "test1");
 	printk("%s\n", buf);
 	kfree(buf);
 
 	asm volatile("int $0");
 	asm volatile("int $0x10");
+	void* page = alloc_page(1);
+	strcpy(page, "test2");
+	printk("%s\n", page);
+	free_page(page, 1);
+	printk("%s\n", page);
+	// if test2 is displayed twice, there was a problem
 }
